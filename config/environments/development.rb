8ca43c6c2e22c98af5e0fdfbdbcc9bb789b9f908
Sidharth <bansal.sidharth2996@gmail.com>
2018-05-25 02:08:24 +0530

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
-  else
+  else 
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
